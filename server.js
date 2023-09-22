f681c67cb7db307d6e900fb56d72443de55195dc
Shubham Mehta <shubham.sm911@gmail.com>
2023-09-22 14:51:46 +0530

    authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const products = require('./db/products')
+const users = require('./db/users');
 const cors = require('cors');
 // creating a server
 
@@ -10,8 +11,14 @@ app.get('/',(req,res)=>{
     res.send('Hello from express App!!!');
 })
 
+//Getting all Products
+
 app.get("/api/products",(req,res)=>{
     res.status(200).json(products);
+})
+
+app.get("/api/auth/login",(req,res)=>{
+    console.log(req);
 })
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
